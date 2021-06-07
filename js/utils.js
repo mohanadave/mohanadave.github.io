047107312e77fd5708f0e79ca92028d887338fe5
mohanadave <45749533+mohanadave@users.noreply.github.com>
2021-06-07 17:39:13 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -9,11 +9,11 @@ if (window.location.href.indexOf('localhost') === -1) {
   url = 'https://shahbhavyan12-oxlf5aqfea-uc.a.run.app';
   directoryName = 'https://mohanadave.github.io/';
 } else {
-   url = 'http://127.0.0.1:8080/';
-  // url = 'https://shahbhavyan12-oxlf5aqfea-uc.a.run.app'
+//    url = 'http://127.0.0.1:8080/';
+  url = 'https://shahbhavyan12-oxlf5aqfea-uc.a.run.app'
   // url = 'https://hasoc-annotation-backend.el.r.appspot.com/';
   // url = "https://hasoc-backend-mgyikj6s3q-as.a.run.app/";
-  directoryName = './bhavyashahh.github.io-main/';
+  directoryName = 'https://mohanadave.github.io/';
   // directoryName = 'https://bhavyashahh.github.io/';
 }
 
