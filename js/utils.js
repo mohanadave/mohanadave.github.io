cedb059f544426bdfbe76f1799ae247f19dd2520
mohanadave <45749533+mohanadave@users.noreply.github.com>
2021-06-07 22:04:54 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -6,11 +6,11 @@ var LANGID=null;
 var TWEET_ID=null;
 if (window.location.href.indexOf('localhost') === -1) {
   // url = 'https://hasoc-annotation-backend.el.r.appspot.com/';
-  url = 'https://hasoc-q3xbfzz3rq-el.a.run.app';
+  url = 'https://git-cd-oxlf5aqfea-uc.a.run.app/';
   directoryName = 'https://mohanadave.github.io//';
 } else {
   //url = 'http://127.0.0.1:8080/';
-  url = 'https://hasoc-q3xbfzz3rq-el.a.run.app';
+  url = 'https://git-cd-oxlf5aqfea-uc.a.run.app/';
   // url = 'https://hasoc-annotation-backend.el.r.appspot.com/';
   // url = "https://hasoc-backend-mgyikj6s3q-as.a.run.app/";
   // directoryName = '/annotationplatform.github.io/';
